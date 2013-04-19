71c1f902256d96d6227b9d5126c24b1bae7e34d7
Gary Turovsky <gary@spantree.net>
2013-04-18 19:34:36 -0500

    Setting up mongo datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -9,6 +9,12 @@ hibernate {
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
+grails {
+	mongo {
+		host = "192.168.121.101"
+		databaseName = "test"
+	}
+}
 // environment specific settings
 environments {
     development {
