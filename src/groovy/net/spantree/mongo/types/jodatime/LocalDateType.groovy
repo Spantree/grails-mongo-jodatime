01de7222cfb4d4a2ff485a23d61c7994bd098bd0
Gary Turovsky <gary@spantree.net>
2013-04-18 19:36:25 -0500

    Implement Equal and Between operations for LocalDate types.  Adding tests.

--- src/groovy/net/spantree/mongo/types/jodatime/LocalDateType.groovy
+++ src/groovy/net/spantree/mongo/types/jodatime/LocalDateType.groovy
@@ -2,55 +2,78 @@ package net.spantree.mongo.types.jodatime
 
 import org.grails.datastore.mapping.engine.types.AbstractMappingAwareCustomTypeMarshaller
 import org.grails.datastore.mapping.model.PersistentProperty
+import org.grails.datastore.mapping.mongo.query.MongoQuery;
 import org.grails.datastore.mapping.query.Query
+import org.grails.datastore.mapping.query.Query.Between
 import org.grails.datastore.mapping.query.Query.Equals
+import org.joda.time.DateTimeZone
 import org.joda.time.LocalDate
 import org.joda.time.ReadablePartial
-import org.joda.time.YearMonth
+import org.joda.time.DateTimeFieldType
 
 import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
 
 class LocalDateType extends AbstractMappingAwareCustomTypeMarshaller<LocalDate, DBObject, DBObject> {
+	
+	static String JODA_TYPE = "LocalDate"
+	
 	LocalDateType() {
 		super(LocalDate)
 	}
 	
 	public DBObject toDBObject(LocalDate value) {
 		[
-			date: value.toDate(),
-			year: value.year,
-			month: value.monthOfYear,
-			quarter: Math.floor((value.monthOfYear-1)/3)+1 as Integer,
-			dayOfMonth: value.dayOfMonth,
-			dayOfWeek: value.dayOfWeek
+			jodaValue: value.toDate(),
+			jodaType: JODA_TYPE
 		] as BasicDBObject
 	}
 	
 	@Override
 	protected Object writeInternal(PersistentProperty property, String key, LocalDate value, DBObject nativeTarget) {
 		if(value) {
-			obj = toDBObject(value)
+			DBObject obj = toDBObject(value)
 			nativeTarget.put(key, obj)
 			return obj
 		}
-		return
+		return null
+	}
+	
+	public Date toDate(Object dtPart) {
+		if(dtPart instanceof ReadablePartial 
+			&& dtPart.isSupported(DateTimeFieldType.monthOfYear())
+			&& dtPart.isSupported(DateTimeFieldType.year())
+			&& dtPart.isSupported(DateTimeFieldType.dayOfMonth())
+			) {
+				
+			new LocalDate(dtPart.get(DateTimeFieldType.year()), dtPart.get(DateTimeFieldType.monthOfYear()), dtPart.get(DateTimeFieldType.dayOfMonth())).toDate()
+		}
 	}
 	
 	@Override
 	protected void queryInternal(PersistentProperty property, String key, Query.PropertyCriterion criterion, DBObject nativeQuery) {
 		if(criterion instanceof Equals) {
-			if(criterion.value instanceof ReadablePartial) {
-				ReadablePartial v = criterion.value
-				if(v instanceof YearMonth || v instanceof LocalDate) {
-					nativeQuery["${key}.year"] = v.year
-					nativeQuery["${key}.month"] = v.monthOfYear
-				}
-				if(v instanceof LocalDate) {
-					nativeQuery["${key}.dayOfMonth"] = v.dayOfMonth
-				}
+			Date dt = toDate(criterion.value)
+			if(dt) { 
+				nativeQuery["${key}.jodaType"] = JODA_TYPE
+				nativeQuery["${key}.jodaValue"] = dt
 			}	
-		} else {
+		} 
+		else if(criterion instanceof Between) {
+			Date fromDt = toDate(criterion.from)
+			Date toDt = toDate(criterion.to)
+		
+			if(fromDt && toDt) {
+				nativeQuery["${key}.jodaType"] = JODA_TYPE
+				
+				DBObject dbo = new BasicDBObject()
+				dbo.put(MongoQuery.MONGO_GTE_OPERATOR, fromDt);
+				dbo.put(MongoQuery.MONGO_LTE_OPERATOR, toDt);
+				
+				nativeQuery["${key}.jodaValue"] = dbo
+			}
+		}
+		else {
 			throw new RuntimeException("unsupported query type for property $property")
 		}
 	}
@@ -58,8 +81,8 @@ class LocalDateType extends AbstractMappingAwareCustomTypeMarshaller<LocalDate, 
 	@Override
 	protected LocalDate readInternal(PersistentProperty property, String key, DBObject nativeSource) {
 	    final value = nativeSource[key]
-	    if(value?.year && value?.month && value?.dayOfMonth) {
-	        return new LocalDate(value.year, value.month, value.dayOfMonth)
+	    if(value?.jodaType == "LocalDate") {
+	        return new LocalDate(value.jodaValue)
 	    }
 	    return null
 	}
--- test/integration/net/spantree/mongo/types/jodatime/LocalDateTypeSpec.groovy
+++ test/integration/net/spantree/mongo/types/jodatime/LocalDateTypeSpec.groovy
@@ -1,7 +1,38 @@
 package net.spantree.mongo.types.jodatime
 
 import grails.plugin.spock.IntegrationSpec
+import org.joda.time.LocalDate
+import spock.lang.Specification
+import com.gmongo.GMongo
+import com.mongodb.DBAddress
+import com.mongodb.ServerAddress
 
 class LocalDateTypeSpec extends IntegrationSpec {
+	def "local date can be persisted and then read back out"() {
+		when:
+			LocalDate dt = new LocalDate().now()
+			
+			LocalDateObject dateObj = new LocalDateObject()
+			dateObj.jodaLocalDate = dt
+			dateObj.save(flush: true)
+			
+			dateObj = LocalDateObject.findByJodaLocalDate(dt)
+		then:
+			assert dateObj?.jodaLocalDate?.equals(dt)
+	}
 	
+	def "local date between query succeeds"() {
+		when:
+			LocalDate now = new LocalDate().now()
+			LocalDate from = now.minusDays(1)
+			LocalDate to = now.plusDays(1)
+			
+			LocalDateObject dateObj = new LocalDateObject()
+			dateObj.jodaLocalDate = now
+			dateObj.save(flush: true)
+			
+			List foundObjs = LocalDateObject.findAllByJodaLocalDateBetween(from,to)
+		then:
+			assert foundObjs
+	}
 }
