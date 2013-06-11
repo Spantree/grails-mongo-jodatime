638e3d924d11382b6b459d707b07d683da9e0098
Roberto Guerra <uris77@gmail.com>
2013-06-10 19:51:15 -0600

    Upgraded Version to 0.1.2
    This version fixex a bug. The interval's start and end time were being
    printed out as the same value.

--- GrailsMongoJodatimeGrailsPlugin.groovy
+++ GrailsMongoJodatimeGrailsPlugin.groovy
@@ -7,7 +7,7 @@ class GrailsMongoJodatimeGrailsPlugin {
 	def groupId = 'net.spantree'
 	
     // the plugin version
-    def version = "0.1"
+    def version = "0.1.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
